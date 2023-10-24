8f1294eb3f40330160af6796147efee656682812
eduildo <51817346+Eduildo@users.noreply.github.com>
2023-10-24 10:28:44 +0100

    Starting with Object-Oriented Programming

---
+++ OOP/multiple-classes/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Number n = new Number();
+        System.out.println(n.result);
+    }
+
+}
---
+++ OOP/multiple-classes/Number.java
@@ -0,0 +1,5 @@
+public class Number {
+    int x = 5;
+    int y = 5;
+    int result = x * y;
+}
