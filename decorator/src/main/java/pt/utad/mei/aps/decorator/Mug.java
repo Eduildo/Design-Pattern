23d0aba6899ebd91118d9bda1fea4e91ff512e51
eduildo <51817346+Eduildo@users.noreply.github.com>
2023-11-09 23:32:10 +0000

    decorator practice

--- decorator/src/main/java/pt/utad/mei/aps/decorator/Mug.java
+++ decorator/src/main/java/pt/utad/mei/aps/decorator/Mug.java
@@ -1,7 +1,17 @@
-package main.java.pt.utad.mei.aps.decorator;
+package pt.utad.mei.aps.decorator;
 
 public class Mug implements ObjetoEstampavel {
     private String _name;
     private double _price;
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public double getPrice() {
+        throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+    }
 
 }
