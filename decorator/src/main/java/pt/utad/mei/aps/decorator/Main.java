300a6013546a1e2cae03fe8b809f94c9e308b45e
eduildo <51817346+Eduildo@users.noreply.github.com>
2023-11-09 23:39:04 +0000

    decorator practice

--- decorator/src/main/java/pt/utad/mei/aps/decorator/Main.java
+++ decorator/src/main/java/pt/utad/mei/aps/decorator/Main.java
@@ -2,6 +2,6 @@ package pt.utad.mei.aps.decorator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ObjetoEstampavel objeto = new Tshirt();
     }
 }
