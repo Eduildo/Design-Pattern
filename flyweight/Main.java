726fb386158b2054595b495a85809115ed95a104
eduildo <51817346+Eduildo@users.noreply.github.com>
2023-11-15 11:51:23 +0000

    Flyweight

--- flyweight/Main.java
+++ flyweight/Main.java
@@ -3,7 +3,19 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Circulo> objetos = new ArrayList<>();
+        ArrayList<Circulo> _objects = new ArrayList<>();
+        
+
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 20, 300));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 20, 300));
+        _objects.add(new Circle(ColorFactory.getColor("black"), 10, 20, 100));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 20, 300));
+        _objects.add(new Circle(ColorFactory.getColor("black"), 15, 20, 70));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 77, 10));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 60, 7));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 20, 300));
+        _objects.add(new Circle(ColorFactory.getColor("red"), 10, 20, 300));
+        
 
     }
 
